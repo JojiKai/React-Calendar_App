29b0c58e1b3a71e5a3ee11415996b62d7624eed4
MENG-KAI <whatever1668@gmail.com>
2025-08-20 23:43:04 +0800

    '2-6_Time:17.30'

--- src/Components/CalendarApp.jsx
+++ src/Components/CalendarApp.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CalendarApp = () => {
+  const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const monthOfYear = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const currentDate = new Date();
+
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // 算出某年某月有幾天，0=「前一個月的最後一天」，再用 .getDate() 取出那一天的日期數字
+
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+  console.log(firstDayOfMonth);
+
   return (
     <div className="calendar-app">
       <div className="calendar">
@@ -13,47 +39,27 @@ const CalendarApp = () => {
             <i className="bx bx-chevron-right"></i>
           </div>
         </div>
+
+        {/*
+         在 JavaScript 裡，.map() 的 callback 會帶三個參數：
+        array.map((value, index, array) => { ... })
+        value 👉 陣列當前元素的值
+        index 👉 當前元素的索引（0,1,2...）
+        array 👉 原始陣列（很少用） 
+        */}
+
         <div className="weekdays">
-          <span>Sun</span>
-          <span>Mon</span>
-          <span>Tue</span>
-          <span>Wed</span>
-          <span>Thu</span>
-          <span>Fri</span>
-          <span>Sat</span>
+          {dayOfWeek.map((day) => (
+            <span key={day}>{day}</span>
+          ))}
         </div>
         <div className="days">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span>7</span>
-          <span>8</span>
-          <span>9</span>
-          <span>10</span>
-          <span>11</span>
-          <span>12</span>
-          <span>13</span>
-          <span>14</span>
-          <span>15</span>
-          <span>16</span>
-          <span className="current-day">17</span>
-          <span>18</span>
-          <span>19</span>
-          <span>20</span>
-          <span>21</span>
-          <span>22</span>
-          <span>23</span>
-          <span>24</span>
-          <span>25</span>
-          <span>26</span>
-          <span>27</span>
-          <span>28</span>
-          <span>29</span>
-          <span>30</span>
-          <span>31</span>
+          {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+            <span key={`empty-${index}`} />
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => (
+            <span key={day + 1}>{day + 1}</span>
+          ))}
         </div>
       </div>
       <div className="events">
@@ -89,7 +95,7 @@ const CalendarApp = () => {
           <div className="event-text">Meeeting with John</div>
           <div className="event-buttons">
             <i className="bx bxs-edit-alt"></i>
-            <i class="bx  bx-message-x"></i>
+            <i className="bx  bx-message-x"></i>
           </div>
         </div>
       </div>
