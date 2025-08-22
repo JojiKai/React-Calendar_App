9c9c4c0d149443e741a7146bc48f2f0c8e41ea3d
MENG KAI <whatever1668@gmail.com>
2025-08-22 11:24:45 +0800

    2-6_Final

--- src/Components/CalendarApp.jsx
+++ src/Components/CalendarApp.jsx
@@ -41,16 +41,30 @@ const CalendarApp = () => {
   // 計算當前月份的第一天是星期幾
   const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
 
+  const prevMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 0 ? prevYear - 1 : prevYear
+    );
+  };
+
+  const nextMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 11 ? prevYear + 1 : prevYear
+    );
+  };
+
   return (
     <div className="calendar-app">
       <div className="calendar">
         <h1 className="heading">Calendar</h1>
         <div className="navigate-date">
-          <h2 className="month">May,</h2>
-          <h2 className="year">2025</h2>
+          <h2 className="month">{monthOfYear[currentMonth]}</h2>
+          <h2 className="year">{currentYear}</h2>
           <div className="buttons">
-            <i className="bx bx-chevron-left"></i>
-            <i className="bx bx-chevron-right"></i>
+            <i className="bx bx-chevron-left" onClick={prevMonth}></i>
+            <i className="bx bx-chevron-right" onClick={nextMonth}></i>
           </div>
         </div>
 
@@ -77,7 +91,18 @@ const CalendarApp = () => {
           ))}
           {/* 產生 1 號到當月最後一天的 <span></span> */}
           {[...Array(daysInMonth).keys()].map((day) => (
-            <span key={day + 1}>{day + 1}</span>
+            <span
+              key={day + 1}
+              className={
+                day + 1 === currentDate.getDate() &&
+                currentMonth === currentDate.getMonth() &&
+                currentYear === currentDate.getFullYear()
+                  ? "current-day"
+                  : ""
+              }
+            >
+              {day + 1}
+            </span>
           ))}
         </div>
       </div>
